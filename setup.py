7c8c2958e31349e743073aae0ea44693887f77bf
Karan <anandkaran707@gmail.com>
2024-12-02 21:09:07 -0600

    Setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ def get_requirements(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace("\n","") for req in requirements]
+        requirements = [req.replace("\n","") for req in requirements if "-e ." not in requirements]
+        requirements = requirements
+        return requirements   
 
 
 setup(
