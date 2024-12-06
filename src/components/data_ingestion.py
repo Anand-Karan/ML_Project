84b71375dc21fd1e416798bdb942f3940e124e9f
Karan <anandkaran707@gmail.com>
2024-12-05 19:33:24 -0600

    Pipeline Built with Flask

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -49,6 +49,7 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e,sys)
+            
         
 if __name__=="__main__":
     obj=DataIngestion()
